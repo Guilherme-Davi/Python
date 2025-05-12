202688f2b73fc6622b7b340b089975fdc4588a33
Guilherme <guilherme.macedo3006@gmail.com>
2025-05-12 08:21:43 -0300

    Update teste.py
    Mudança na média de .1f para .2f

--- teste.py
+++ teste.py
@@ -4,7 +4,7 @@ lista = [10, 23, 5, 8, 12, 33, 42, 7, 19, 28, 3, 16, 9, 50, 21]
 
 media, maior, menor, pares = analisar_lista(lista)
 
-print(f"Média: {media:.1f}")
+print(f"Média: {media:.2f}")
 print(f"Maior número: {maior}")
 print(f"Menor número: {menor}")
 print(f"Quantidade de pares: {pares}")
