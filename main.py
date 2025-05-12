c1d6333afaff90da4285495de099f9832dfa4838
Guilherme <guilherme.macedo3006@gmail.com>
2025-05-12 08:20:36 -0300

    Update main.py
    Correção pares, estava sem o [], len não funciona diretamente com generators

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ def analisar_lista(lista):
     media = sum(lista) / len(lista)
     maior = max(lista)
     menor = min(lista)
-    pares = len(num for num in lista if num % 2 == 0)
+    pares = len([num for num in lista if num % 2 == 0])
     
     return media, maior, menor, pares
