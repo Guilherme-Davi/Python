b578c4cc035dfccf6caf6433b1ae0b7d2acc1ccb
Guilherme <guilherme.macedo3006@gmail.com>
2025-05-12 08:03:34 -0300

    Create main.py
    Função que analisa uma lista de números inteiros.

---
+++ main.py
@@ -0,0 +1,7 @@
+def analisar_lista(lista):
+    media = sum(lista) / len(lista)
+    maior = max(lista)
+    menor = min(lista)
+    pares = len(num for num in lista if num % 2 == 0)
+    
+    return media, maior, menor, pares
